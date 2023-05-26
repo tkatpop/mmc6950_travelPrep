56e754edc254017ceafdc02587cafad0451b1cd3
tkatpop <93983050+tkatpop@users.noreply.github.com>
2023-05-26 15:02:49 -0400

    started header/footer template, CSS reset, js file

---
+++ templates/header.php
@@ -0,0 +1,23 @@
+<?php
+
+    //Include functions.php file
+    include('includes/functions.php'); 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/style.css" media="all"/>
+        <title>TravelPrep</title>
+    </head>
+
+    <header>
+        <nav><!--Add the nav bar after they login--></nav>
+    </header>
+
+    <body>
+       <div id="container">
+        
+    <!--This is where the content goes-->
