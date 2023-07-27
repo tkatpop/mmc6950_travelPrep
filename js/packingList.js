081beb26bfe3fb844222e95a2979a08adacdc434
tkatpop <93983050+tkatpop@users.noreply.github.com>
2023-07-26 22:30:42 -0400

    removed unness. js

--- js/packingList.js
+++ js/packingList.js
@@ -1,13 +1,9 @@
 //Packing List Season Dropdown Selection
 
 var packingForm = document.getElementById('packingForm')
-// var selectSeason = document.getElementById('season')
+
 var packingListDisplay = document.getElementById('Packing-List-Display')
-// var selectSpring = document.getElementById('Spring')
-// var selectSummer = document.getElementById('Summer')
-// var selectFall = document.getElementById('Fall')
-// var selectWinter = document.getElementById('Winter')
-// var selectChoose = document.getElementById('Choose')
+
 
 document.getElementById('season').onchange = function seasonList(){
 
@@ -34,28 +30,3 @@ document.getElementById('season').onchange = function seasonList(){
   }
 
 }
-
-//Hamburger Menu
-const menu = document.querySelector(".hamburger-menu");
-const menuItems = document.querySelectorAll(".menuItem");
-const hamburger = document.querySelector(".hamburger-btn");
-const closeIcon = document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
-
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-  }
-}
-
-hamburger.addEventListener("click", toggleMenu);
-
-menuItems.forEach(function (menuItem) {
-  menuItem.addEventListener("click", toggleMenu);
-});
